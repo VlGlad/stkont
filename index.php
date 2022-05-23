1b424a46691a82b57b6c0a732477ad31c629e8f2
VlGlad <kva-2001@yandex.ru>
2022-05-23 19:05:11 +0500

    Упростил хедер

--- index.php
+++ index.php
@@ -2,6 +2,27 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 ?>
 
+        <div class="index-section-1">
+            <div class="container relative">
+                <nav class="main-nav -flex -center -just-btw">
+
+                </nav>
+                <div class="index-section-1__content">
+                    <h1>Продажа и обслуживание
+                        складской техники
+                        напрямую со склада производителя</h1>
+                    <div class="index-section-1__des">
+                        Подберем технику за 15 минут <br>
+                        на основании особенностей эксплуатации
+                    </div>
+
+                </div>
+                <div class="kube-1">
+                    <img src="<?=SITE_TEMPLATE_PATH?>/img/ku.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
